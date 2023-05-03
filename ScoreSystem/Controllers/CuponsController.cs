f090a23ef96c5212bf1685094cde5be74d22882f
samyrosa <sr.oliveira0910@gmail.com>
2023-05-03 09:48:46 -0300

    Criação do Controller de Cupons e da view de Manutenção

---
+++ ScoreSystem/Controllers/CuponsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScoreSystem.Controllers
+{
+    public class CuponsController : Controller
+    {
+        public IActionResult Lista()
+        {
+            return View();
+        }
+    }
+}
