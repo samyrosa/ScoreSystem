7cbbfdcfa174790e4b7e811ff04b75ce9a09e201
Joo1480 <60790067+Joo1480@users.noreply.github.com>
2023-05-04 08:58:48 -0300

    View e Controllers de Recompensas

---
+++ ScoreSystem/Controllers/RecompensasController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScoreSystem.Controllers
+{
+    public class RecompensasController : Controller
+    {
+        public IActionResult Lista()
+        {
+            return View();
+        }
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
