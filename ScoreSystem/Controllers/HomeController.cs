a662ffa8b252719302206759e2fd6618c064bf9f
samyrosa <sr.oliveira0910@gmail.com>
2023-06-06 23:03:22 -0300

    Pagina acesso negado

--- ScoreSystem/Controllers/HomeController.cs
+++ ScoreSystem/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace ScoreSystem.Controllers
             return View();
         }
 
+        public IActionResult AcessoNegado()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
